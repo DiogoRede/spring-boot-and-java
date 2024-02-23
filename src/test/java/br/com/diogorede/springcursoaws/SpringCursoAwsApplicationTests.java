f4fcdcf0231220c21c3730e03f70bdd9ca64e15a
Diogo Rede <diogorede2021@gmail.com>
2024-02-23 16:18:19 -0300

    feat: adicionando teste de integração para nossa api verificar o swagger com rest-assured

--- src/test/java/br/com/diogorede/springcursoaws/SpringCursoAwsApplicationTests.java
+++ src/test/java/br/com/diogorede/springcursoaws/SpringCursoAwsApplicationTests.java
@@ -1,13 +1,12 @@
 package br.com.diogorede.springcursoaws;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCursoAwsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
