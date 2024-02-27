b61b0e4c2bbf9c4906681c4672a5fc93605b8282
Diogo Rede <diogorede2021@gmail.com>
2024-02-27 18:29:02 -0300

    feat: adicionando tabelas para segurança da API

---
+++ src/main/resources/db/migration/V9__Create_Table_Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  user_name TEXT,
+  full_name TEXT,
+  password TEXT,
+  account_non_expired BOOLEAN DEFAULT NULL,
+  account_non_locked BOOLEAN DEFAULT NULL,
+  credentials_non_expired BOOLEAN DEFAULT NULL,
+  enabled BOOLEAN DEFAULT NULL,
+  UNIQUE (user_name)
+);
