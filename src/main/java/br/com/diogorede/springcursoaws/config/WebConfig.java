c8ee19c8b1119634940a96bbe4c26b47a1e18598
Diogo Rede <diogorede2021@gmail.com>
2024-02-27 18:27:31 -0300

    feat: configurando CORS

--- src/main/java/br/com/diogorede/springcursoaws/config/WebConfig.java
+++ src/main/java/br/com/diogorede/springcursoaws/config/WebConfig.java
@@ -1,12 +1,27 @@
 package br.com.diogorede.springcursoaws.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
+
+    @Value("${cors.originPatters:default}")
+    private String cors = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = cors.split(",");
+        registry.addMapping("/**")
+            // .allowedMethods("GET","POST","PUT", "DELETE")
+            .allowedMethods("*")
+            .allowedOrigins(allowedOrigins)
+            .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
