e36b886876908837979c4aa7d567d8cfee184949
DiogoRede <diogorede2021@gmail.com>
2024-03-09 21:35:35 -0300

    fix: arrumando configuração do spring security

--- src/main/java/br/com/diogorede/springcursoaws/config/SecurityConfig.java
+++ src/main/java/br/com/diogorede/springcursoaws/config/SecurityConfig.java
@@ -1,32 +1,46 @@
 package br.com.diogorede.springcursoaws.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import br.com.diogorede.springcursoaws.security.jwt.JwtConfigurer;
+import br.com.diogorede.springcursoaws.security.jwt.JwtTokenProvider;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
- 
+
+    @Autowired
+    private JwtTokenProvider provider;    
+
+    public SecurityConfig(JwtTokenProvider provider) {
+        this.provider = provider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf -> csrf.disable())
+                   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                    .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/signin", "/auth/refresh",
-                                                     "/swagger-ui.html**").permitAll()
-                    
+                        .requestMatchers("/auth/signin", "/auth/refresh/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html**").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .requestMatchers("/users").denyAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().denyAll()
                     )
-                    // .cors()
-                    // .apply(new JwtConfigurer(provider))
+                    .cors()
+                    .and()
+                    .apply(new JwtConfigurer(provider))
+                    .and()
                    .build();
     }
 
