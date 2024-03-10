51e8b193ce0733200ede0e612f4c451e4565d011
DiogoRede <diogorede2021@gmail.com>
2024-03-09 21:36:39 -0300

    fix: ajustando anyRequest para authenticated

--- src/main/java/br/com/diogorede/springcursoaws/config/SecurityConfig.java
+++ src/main/java/br/com/diogorede/springcursoaws/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html**").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .requestMatchers("/users").denyAll()
-                        .anyRequest().denyAll()
+                        .anyRequest().authenticated()
                     )
                     .cors()
                     .and()
